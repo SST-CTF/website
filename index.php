6d0a4bc46e285c844a646c7e7884723f8e590560
Otakar Andrysek <otakar@me.com>
2017-01-09 14:23:01 -0800

    Server Update | 2017-01-09 14:23:01

--- index.php
+++ index.php
@@ -561,15 +561,15 @@ if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !
                             <span>Previous Meeting</span>
                             <div class="value">
                                 <span></span>
-                                <span><!--PreviousDateBegin-->01/03/17<!--PreviousDateEnd--></span><br>
-                                <span><!--PreviousDateLongBegin-->Tuesday January 03 (2:15-3:30)<!--PreviousDateLongEnd--></span>
+                                <span><!--PreviousDateBegin-->01/05/17<!--PreviousDateEnd--></span><br>
+                                <span><!--PreviousDateLongBegin-->Thursday January 05 (2:15-3:30)<!--PreviousDateLongEnd--></span>
                             </div>
                             <ul>
-                                <li><!--PreviousTopicBegin-->Topic: Competition Overview<!--PreviousTopicEnd--></li>
+                                <li><!--PreviousTopicBegin-->Topic: State of the Club<!--PreviousTopicEnd--></li>
                                 <li><!--PreviousLeaderBegin-->Leader: Otakar Andrysek<!--PreviousLeaderEnd--></li>
-                                <li><!--PreviousDescriptionBegin-->Description: Discussed writeups of previous competition, and prepared for Insomni'hack.<!--PreviousDescriptionEnd--></li>
-                                <li><!--PreviousHomeworkBegin-->Homework: 3DSCTF writeups<!--PreviousHomeworkEnd--></li>
-                                <li><!--PreviousDownloadBegin--><a href="downloads/01-03-17"><!--PreviousDownloadEnd-->Downloads</a></li>
+                                <li><!--PreviousDescriptionBegin-->Description: Discuss and debate the future of SST-CTF.<!--PreviousDescriptionEnd--></li>
+                                <li><!--PreviousHomeworkBegin-->Homework: None<!--PreviousHomeworkEnd--></li>
+                                <li><!--PreviousDownloadBegin--><a href="downloads/01-05-17"><!--PreviousDownloadEnd-->Downloads</a></li>
                             </ul>
                         </div>
                     </div>
@@ -579,15 +579,15 @@ if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !
                             <span>Next Meeting</span>
                             <div class="value">
                                 <span></span>
-                                <span><!--NextDateBegin-->01/05/17<!--NextDateEnd--></span><br>
-                                <span><!--NextDateLongBegin-->Thursday January 05 (2:15-3:30)<!--NextDateLongEnd--></span>
+                                <span><!--NextDateBegin-->01/09/17<!--NextDateEnd--></span><br>
+                                <span><!--NextDateLongBegin-->Monday January 09 (2:15-3:30)<!--NextDateLongEnd--></span>
                             </div>
                             <ul>
-                                <li><!--NextTopicBegin-->Topic: State of the Club<!--NextTopicEnd--></li>
-                                <li><!--NextLeaderBegin-->Leader: Otakar Andrysek<!--NextLeaderEnd--></li>
-                                <li><!--NextDescriptionBegin-->Description: Discuss and debate the future of SST-CTF.<!--NextDescriptionEnd--></li>
-                                <li><!--NextHomeworkBegin-->Homework: None<!--NextHomeworkEnd--></li>
-                                <li><!--NextDownloadBegin--><a href="downloads/01-05-17"><!--NextDownloadEnd-->Downloads</a></li>
+                                <li><!--NextTopicBegin-->Topic: GitHub and Projects<!--NextTopicEnd--></li>
+                                <li><!--NextLeaderBegin-->Leader: Andrew Quach<!--NextLeaderEnd--></li>
+                                <li><!--NextDescriptionBegin-->Description: Refresh lesson on GitHub, work on typing-test, prepare for competition.<!--NextDescriptionEnd--></li>
+                                <li><!--NextHomeworkBegin-->Homework: Projects<!--NextHomeworkEnd--></li>
+                                <li><!--NextDownloadBegin--><a href="downloads/01-09-17"><!--NextDownloadEnd-->Downloads</a></li>
                             </ul>
                         </div>
                     </div>
@@ -597,15 +597,15 @@ if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !
                             <span>Future Meeting</span>
                             <div class="value">
                                 <span></span>
-                                <span><!--FutureDateBegin-->01/09/17<!--FutureDateEnd--></span><br>
-                                <span><!--FutureDateLongBegin-->Monday January 09 (2:15-3:30)<!--FutureDateLongEnd--></span>
+                                <span><!--FutureDateBegin-->01/13/17<!--FutureDateEnd--></span><br>
+                                <span><!--FutureDateLongBegin-->Friday January 13 (2:15-3:30)<!--FutureDateLongEnd--></span>
                             </div>
                             <ul>
-                                <li><!--FutureTopicBegin-->Topic: GitHub and Projects<!--FutureTopicEnd--></li>
-                                <li><!--FutureLeaderBegin-->Leader: Andrew Quach<!--FutureLeaderEnd--></li>
-                                <li><!--FutureDescriptionBegin-->Description: Refresh lesson on GitHub, work on typing-test, prepare for competition.<!--FutureDescriptionEnd--></li>
-                                <li><!--FutureHomeworkBegin-->Homework: Projects<!--FutureHomeworkEnd--></li>
-                                <li><!--FutureDownloadBegin--><a href="downloads/01-09-17"><!--FutureDownloadEnd-->Downloads</a></li>
+                                <li><!--FutureTopicBegin-->Topic: Mentor/Funding<!--FutureTopicEnd--></li>
+                                <li><!--FutureLeaderBegin-->Leader: Otakar Andrysek<!--FutureLeaderEnd--></li>
+                                <li><!--FutureDescriptionBegin-->Description: Work on getting sponsors and a mentor.<!--FutureDescriptionEnd--></li>
+                                <li><!--FutureHomeworkBegin-->Homework: Finish funding proposal<!--FutureHomeworkEnd--></li>
+                                <li><!--FutureDownloadBegin--><a href="downloads/01-13-17"><!--FutureDownloadEnd-->Downloads</a></li>
                             </ul>
                         </div>
                     </div>
