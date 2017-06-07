6e42ce63f5838ad123d217d3cc0ac3db997bf840
Otakar Andrysek <otakar@me.com>
2017-06-07 00:22:01 -0700

    Server Update | 2017-06-07 00:22:01

--- mailtest.php
+++ mailtest.php
@@ -1,5 +1,5 @@
 <?php
 
-mail("otakar@sstctf.org", "TEST1", "TEST2");
+mail("otakar@sstctf.org", "This is the easiest time of your life...", "You are not living up to your fullest potential.");
 
 ?>
